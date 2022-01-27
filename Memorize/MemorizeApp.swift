78439bc604627b86a7280bae9422289cb058ef7f
Jonny Johansson <jonny.k.johansson@gmail.com>
2022-01-27 18:14:12 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jonny Johansson on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
